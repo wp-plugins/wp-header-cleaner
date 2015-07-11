e978058abb70d089d0f8dba5744650e6565800f8
chandrakeshkumar <chandrakeshkumar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-11 05:10:56 +0000

    Easy way to clean unwanted links from header & footer.
    git-svn-id: https://plugins.svn.wordpress.org/wp-header-cleaner/trunk@1196831 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== WP Header Cleaner ===
+Contributors: chandrakeshkumar,chandgiri67,wpchandra
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl.html
+Tags:  clean,head header cleaner, wordpress
+Requires at least: 3.4
+Tested up to: 4.2.2
+Stable tag: 0.3
+
+Easy way to clean unwanted links from header & footer.
+
+== Description ==
+
+Easy way to clean unwanted links from header & footer.
+
+
+
+== Installation ==
+
+1. Upload the `wp-clean-header` folder to the `/wp-content/plugins/` directory
+2. Activate the  WP Header Cleaner plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+
+== Changelog ==
+
+
+= 1.0 =
+
+* Initial release.
---
+++ wp-clean-header.php
@@ -0,0 +1,56 @@
+<?php
+defined( 'ABSPATH' ) OR exit;
+
+/*
+Plugin Name: WP Header Cleaner
+Plugin URI: http://wordpress.org/extend/plugins/wp-header-cleaner/
+Description: Easy way to clean unwanted links from header & footer.
+Version: 1.0
+Author: Chandrakesh Kumar
+Author URI: http://www.wpchandra.com/
+Tags: clean,head header cleaner, wordpress
+*/
+
+// ===============================  WP Header Cleaner  ======================================
+class WP_Clean_Header {
+	
+	public function __construct(){
+       add_action('init', array( $this, 'cleanup_head' ));
+    }
+	 // WordPress cleanup function
+	public function cleanup_head() {
+	remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+	remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+	remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+	remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+	remove_action( 'wp_head', 'index_rel_link' ); // index link
+	remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+	remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+	remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+	remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
+	remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);    // #5
+    add_filter('the_generator', '__return_false');            // #6
+    add_filter('show_admin_bar','__return_false');            // #7
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );  // #8
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	}
+	  
+}
+$wp_header_cleaner = new WP_Clean_Header();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
